1fb5c5a831ee2338e43e1488d928f73b63d5d8b5
nasirs-GitHub <nwilliams7123@gmail.com>
2025-05-07 23:00:27 -0400

    implemented past practice exercise collision system

--- test_map/test_map/Game1.cs
+++ test_map/test_map/Game1.cs
@@ -14,13 +14,10 @@ namespace test_map
 
         private Rectangle player;
 
-        private List<Rectangle> tops;
-        private List<Rectangle> lefts;
-        private List<Rectangle> bottoms;
-        private List<Rectangle> rights;
+        private Rectangle firstRect;
+        private Rectangle secondRect;
 
-        private Rectangle firstRoom;
-        private Rectangle secondRoom;
+        private List<Rectangle> boundaries;
 
         public Game1()
         {
@@ -31,30 +28,19 @@ namespace test_map
 
         protected override void Initialize()
         {
-            System.Diagnostics.Debug.WriteLine(
-                $"Window Width: {_graphics.PreferredBackBufferWidth}" +
-                $"\nWindow Height: {_graphics.PreferredBackBufferHeight}");
+            player = new Rectangle(395, 235, 5, 5);
 
-            tops = new List<Rectangle>();
-            tops.Add(new Rectangle(350, 240, 100, 1));
-            tops.Add(new Rectangle(450, 265, 50, 1));
+            firstRect = new Rectangle(350, 190, 100, 100);
+            secondRect = new Rectangle(450, 215, 50, 50);
 
-            lefts = new List<Rectangle>();
-            lefts.Add(new Rectangle(350, 240, 1, 100));
+            boundaries = new List<Rectangle>();
+            boundaries.Add(new Rectangle(350, 165, 100, 25));
+            boundaries.Add(new Rectangle(325, 190, 25, 100));
+            boundaries.Add(new Rectangle(350, 290, 100, 25));
 
-            bottoms = new List<Rectangle>();
-            bottoms.Add(new Rectangle(350, 340, 100, 1));
-            bottoms.Add(new Rectangle(450, 315, 50, 1));
-
-            rights = new List<Rectangle>();
-            rights.Add(new Rectangle(450, 240, 1, 25));
-            rights.Add(new Rectangle(450, 315, 1, 25));
-            rights.Add(new Rectangle(500, 265, 1, 50));
-
-            firstRoom = new Rectangle(350, 240, 100, 100);
-            secondRoom = new Rectangle(450, 265, 50, 50);
-
-            player = new Rectangle(360, 250, 1, 1);
+            boundaries.Add(new Rectangle(450, 265, 50, 25));
+            boundaries.Add(new Rectangle(500, 215, 25, 50));
+            boundaries.Add(new Rectangle(450, 190, 50, 25));
 
             base.Initialize();
         }
@@ -71,53 +57,8 @@ namespace test_map
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            if (Keyboard.GetState().IsKeyDown(Keys.W))
-            {
-                foreach(Rectangle side in tops)
-                {
-                    if (player.Top >= side.Top)
-                    {
-                        player.Y -= 1;
-                    }
-                }
-            }
-
-            else if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {
-                foreach (Rectangle side in lefts)
-                {
-                    if (player.Left >= side.Left)
-                    {
-                        player.X -= 1;
-                    }
-                }
-            }
-
-            else if (Keyboard.GetState().IsKeyDown(Keys.S))
-            {
-                foreach (Rectangle side in bottoms)
-                {
-                    if (player.Bottom <= side.Bottom)
-                    {
-                        player.Y += 1;
-                    }
-                }    
-            }
-
-            else if (Keyboard.GetState().IsKeyDown(Keys.D))
-            {
-                if (
-                    (player.Right <= rights[0].Right && 
-                    player.Right <= rights[1].Right) ||  
-                    player.Right <= rights[2].Right)
-                {
-                    player.X += 1;
-                }
-            }
-
-            System.Diagnostics.Debug.WriteLine(
-                $"player right: {player.Right}" +
-                $"\ntop third right: {rights[0].Right}");
+            Collide();
+            Move();
 
             base.Update(gameTime);
         }
@@ -127,33 +68,82 @@ namespace test_map
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(pixel, firstRoom, Color.White);
-            _spriteBatch.Draw(pixel, secondRoom, Color.White);
+            //_spriteBatch.Draw(pixel, firstRect, Color.Red);
+            //_spriteBatch.Draw(pixel, secondRect, Color.Blue);
 
-            foreach (Rectangle side in tops)
+            foreach (Rectangle boundary in boundaries)
             {
-                _spriteBatch.Draw(pixel, side, Color.Black);
+                _spriteBatch.Draw(pixel, boundary, Color.Yellow);
             }
 
-            foreach (Rectangle side in lefts)
-            {
-                _spriteBatch.Draw(pixel, side, Color.Black);
-            }
-
-            foreach (Rectangle side in bottoms)
-            {
-                _spriteBatch.Draw(pixel, side, Color.Black);
-            }
-
-            foreach (Rectangle side in rights)
-            {
-                _spriteBatch.Draw(pixel, side, Color.Black);
-            }
-
-            _spriteBatch.Draw(pixel, player, Color.Red);
+            _spriteBatch.Draw(pixel, player, Color.Black);
 
             _spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        public void Move()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                player.Y -= 1;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                player.X -= 1;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                player.Y += 1;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                player.X += 1;
+            }
+        }
+
+        public void Collide()
+        {
+            List<Rectangle> collisions = new List<Rectangle>();
+
+            foreach (Rectangle boundary in boundaries)
+            {
+                if (boundary.Intersects(player))
+                {
+                    collisions.Add(boundary);
+                }
+            }
+
+            foreach(Rectangle boundary in collisions)
+            {
+                Rectangle overlap = Rectangle.Intersect(boundary, player);
+
+                if (overlap.Height >= overlap.Width)
+                {
+                    if (player.X < boundary.X)
+                    {
+                        player.X -= overlap.Width;
+                    }
+
+                    else
+                    {
+                        player.X += overlap.Width;
+                    }
+                }
+
+                else
+                {
+                    if (player.Y < boundary.Y)
+                    {
+                        player.Y -= overlap.Height;
+                    }
+
+                    else
+                    {
+                        player.Y += overlap.Height;
+                    }
+                }
+            }
         }
     }
 }
