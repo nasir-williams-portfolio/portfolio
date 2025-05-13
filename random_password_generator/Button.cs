e71080c9baa50d3bc7866a1139935faad30b8597
nasirs-GitHub <nwilliams7123@gmail.com>
2025-05-13 16:16:54 -0400

    created the base model, can randomize a ten character long password

--- random_password_generator/Button.cs
+++ random_password_generator/Button.cs
@@ -1,6 +1,73 @@
-﻿namespace random_password_generator
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace random_password_generator
 {
+
+    public delegate void OnButtonClickDelegate();
+
     internal class Button
     {
+        private Texture2D sprite;
+        private Texture2D sprite_hover;
+        private Rectangle dest_rectangle;
+
+        private Texture2D current_sprite;
+
+        private MouseState curr_mouse;
+        private MouseState prev_mouse;
+
+        public event OnButtonClickDelegate OnButtonClick;
+
+        public Button(Texture2D sprite, Texture2D sprite_hover, Vector2 dest_vector)
+        {
+            this.sprite = sprite;
+            this.sprite_hover = sprite_hover;
+
+            this.dest_rectangle = new Rectangle(
+                (int)dest_vector.X,
+                (int)dest_vector.Y,
+                sprite.Width,
+                sprite.Height);
+
+
+            prev_mouse = curr_mouse;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(
+                current_sprite,
+                dest_rectangle,
+                Color.White);
+        }
+
+        public void Update()
+        {
+            curr_mouse = Mouse.GetState();
+
+            Rectangle mouse_rectangle = new Rectangle(curr_mouse.X, curr_mouse.Y, 1, 1);
+
+            if ((curr_mouse.LeftButton == ButtonState.Pressed && prev_mouse.LeftButton == ButtonState.Released))
+            {
+                if (OnButtonClick != null)
+                {
+                    OnButtonClick();
+                }
+            }
+
+            if (dest_rectangle.Contains(mouse_rectangle) && curr_mouse.LeftButton == ButtonState.Pressed)
+            {
+                current_sprite = sprite_hover;
+            }
+
+            else
+            {
+                current_sprite = sprite;
+            }
+
+            prev_mouse = curr_mouse;
+        }
     }
 }
--- random_password_generator/Game1.cs
+++ random_password_generator/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace random_password_generator
 {
@@ -10,6 +11,18 @@ namespace random_password_generator
         private SpriteBatch _spriteBatch;
 
         private SpriteFont font;
+        private Texture2D sprite;
+        private Texture2D sprite_hover;
+
+        private Button button;
+
+        private string text;
+        private string[] letters;
+        private int[] numbers;
+        private Random rng;
+
+        private int window_height;
+        private int window_width;
 
         public Game1()
         {
@@ -20,6 +33,13 @@ namespace random_password_generator
 
         protected override void Initialize()
         {
+            letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+            numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            text = "";
+            rng = new Random();
+            window_height = _graphics.PreferredBackBufferHeight;
+            window_width = _graphics.PreferredBackBufferWidth;
+
             base.Initialize();
         }
 
@@ -28,28 +48,57 @@ namespace random_password_generator
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             font = Content.Load<SpriteFont>("daydream_8");
+            sprite = Content.Load<Texture2D>("button");
+            sprite_hover = Content.Load<Texture2D>("button_hover");
+
+            button = new Button(
+                sprite,
+                sprite_hover,
+                new Vector2(
+                    window_width / 2 - 31,
+                    window_height / 2 + 35));
+
+            button.OnButtonClick += this.RandomizePassword;
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            button.Update();
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            Vector2 position = new Vector2(
+                window_width / 2 - font.MeasureString(text).X / 2,
+                window_height / 2 - font.MeasureString(text).Y);
+
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(font, "Hello, World!", Vector2.Zero, Color.Black);
+
+            _spriteBatch.DrawString(
+                font,
+                text,
+                position,
+                Color.Black);
+            button.Draw(_spriteBatch);
+
             _spriteBatch.End();
             base.Draw(gameTime);
         }
 
-        public string RandomizePassword()
+        public void RandomizePassword()
         {
-            string password = "";
-            return password;
+            text = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                text += letters[rng.Next(0, 26)];
+            }
         }
     }
 }
