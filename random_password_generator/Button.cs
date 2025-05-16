d32bbcc8af6cc1d65050cbe17e43a4f3e784c90e
nasirs-GitHub <nwilliams7123@gmail.com>
2025-05-16 15:16:14 -0400

    condensed for simplicity

--- random_password_generator/Button.cs
+++ random_password_generator/Button.cs
@@ -18,6 +18,7 @@ namespace random_password_generator
         private MouseState curr_mouse;
         private MouseState prev_mouse;
         private Rectangle click_position;
+        private Rectangle mouse_rectangle;
 
         public event OnButtonClickDelegate OnButtonClick;
 
@@ -38,6 +39,7 @@ namespace random_password_generator
             prev_mouse = curr_mouse;
             current_sprite = sprite;
             click_position = new Rectangle(0, 0, 1, 1);
+            mouse_rectangle = new Rectangle(0, 0, 1, 1);
         }
 
         public void Draw(SpriteBatch sb)
@@ -51,19 +53,19 @@ namespace random_password_generator
         public void Update()
         {
             curr_mouse = Mouse.GetState();
-
-            Rectangle mouse_rectangle = new Rectangle(curr_mouse.X, curr_mouse.Y, 1, 1);
-
-            if (curr_mouse.LeftButton == ButtonState.Pressed && prev_mouse.LeftButton == ButtonState.Released)
-            {
-                click_position = new Rectangle((int)curr_mouse.X, (int)curr_mouse.Y, 1, 1);
-            }
+            mouse_rectangle = new Rectangle(curr_mouse.X, curr_mouse.Y, 1, 1);
 
             if (dest_rectangle.Contains(mouse_rectangle))
             {
-                if ((curr_mouse.LeftButton == ButtonState.Pressed && prev_mouse.LeftButton == ButtonState.Released) && OnButtonClick != null)
+                if (curr_mouse.LeftButton == ButtonState.Pressed && prev_mouse.LeftButton == ButtonState.Released)
                 {
-                    OnButtonClick();
+                    if (OnButtonClick != null)
+                    {
+                        OnButtonClick();
+                    }
+
+                    click_position.X = (int)curr_mouse.X;
+                    click_position.Y = (int)curr_mouse.Y;
                 }
 
                 if (curr_mouse.LeftButton == ButtonState.Pressed && dest_rectangle.Contains(click_position))
