a63d39bc365110509872f9688cbc0ed4050ae1cc
nasirs-GitHub <nwilliams7123@gmail.com>
2026-08-02 15:29:50 -0400

    Added Game Window Class

---
+++ HackYourSummerProjectTwo/GameModeWindow.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HackYourSummerProjectTwo
+{
+    internal class GameModeWindow
+    {
+        private Texture2D texture;
+        private Rectangle destinationRectangle;
+
+        public GameModeWindow(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+
+        }
+    }
+}
